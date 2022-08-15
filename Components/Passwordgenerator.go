a26759e9c5d1a49f66b04a5f4269fae2e9efb7f9
adam <adamharits25@gmail.com>
2022-08-15 10:29:25 +0700

    first commit

---
+++ Components/Passwordgenerator.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"crypto/sha256"
+	"encoding/base64"
+	"fmt"
+)
+
+func encryptPassword(password string) string {
+	h := sha256.New()
+	h.Write([]byte(password))
+	b := h.Sum(nil)
+	return "{SHA256}" + base64.StdEncoding.EncodeToString(b)
+}
+
+func encryptPassword2(password string) string {
+	h := sha256.Sum256([]byte(password))
+	return "{SHA256}" + base64.StdEncoding.EncodeToString(h[:])
+}
+
+func main() {
+	fmt.Println(encryptPassword("iniPassword..."))
+	fmt.Println(encryptPassword2("abcd1234"))
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module golang/pass_gen
+
+go 1.19
